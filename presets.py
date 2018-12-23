8c6fce8b42d7a9e5ede5184f2bcc9eb7e4acfd0d
artsun <parafo@gmail.com>
2018-12-23 21:50:25 +0300

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,12 @@
+# CONSTANTS
+
+# for VK
+VERSION = 5.52
+
+# for server
+WEBDIR = "shell"
+USER_DATA_FILE = "shell\\user_data.js"
+
+
+def set_presets(base_id=None, mytoken=None, depth=None):
+    return base_id, mytoken, depth
